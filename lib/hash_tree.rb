64a53589dbc85415a41ef45115835a7c37a2bc1e
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-07-08 23:14:05 +0200

    Looking good

--- lib/hash_tree.rb
+++ lib/hash_tree.rb
@@ -10,26 +10,21 @@ require "indented_io"
 #   o HashTree::Set, HashTree::Map, HashTree::Array
 #
 module HashTree
-  class Set
+  class Node
     attr_reader :parent
     attr_reader :children
-    attr_reader :key
 
     def initialize(parent, key)
-      @parent, @children, @key = parent, {}, key
-      @parent&.send(:attach, self)
+      @children = {}
+      parent&.do_attach(key, self)
     end
+    
+    def attach(key, child) do_attach(key, child) end
 
-    def attach(child)
-      !@children.key?(child.key) or raise "Duplicate child key: #{child.key.inspect}"
-      child.instance_variable_set(:@parent, self)
-      @children[child.key] = child
-      child.send(:clear_cached_properties)
-    end
-
-    def detach(key)
-      child = children[key]
+    def detach(key, ignore_not_attached: false)
       @children.key?(key) or raise "Non-existing child key: #{key.inspect}"
+      child = children[key]
+      ignore_not_attached || child.parent or raise "Child is not attached"
       child.instance_variable_set(:@parent, nil)
       @children.delete(key)
       child.send(:clear_cached_properties)
@@ -53,16 +48,6 @@ module HashTree
       (@ancestors ||= parents(false).reverse) + (include_self ? [self] : [])
     end
 
-    # Unique dot-separated list of keys leading from the root object to 
-    # self. Note that the root object is not included in the path so that
-    #
-    #   obj.parent.nil? || obj.root.dot(obj.path) == obj
-    #
-    # is always true
-    #
-    # Note that for this to work, keys may not contain a dots ('.')
-    def path() @path ||= ancestors(true)[1..-1].join(".") end
-
     # Recursively lookup object by dot-separated list of names
     #
     # Note that for this to work, keys may not contain a dots ('.')
@@ -70,11 +55,14 @@ module HashTree
       path.split(".").inject(self) { |a,e| a[e] or raise "Can't lookup '#{e}' in #{a.path.inspect}" }
     end
 
-    # Compare two objects by path
-    def <=>(r) path <=> r.path end
-
-    # A node is rendered as its key
-    def to_s() key.to_s end
+  protected
+    def do_attach(key, child)
+      !@children.key?(key) or raise "Duplicate child key: #{key.inspect}"
+      !child.parent or raise "Child is already attached"
+      child.instance_variable_set(:@parent, self)
+      @children[key] = child
+      child.send(:clear_cached_properties)
+    end
 
   private
     # Recursively clear cached properties like @parents in each node in the
@@ -94,6 +82,42 @@ module HashTree
         children.values.each { |c| c.clear_cached_properties }
       end
     end
+  end
+
+  class Set < Node
+    alias node_attach attach
+
+    attr_reader :key
+
+    def initialize(parent, key)
+      @key = key
+      super(parent, key)
+    end
+
+    def attach(child) do_attach(child.key, child) end
+
+    # Unique dot-separated list of keys leading from the root object to 
+    # self. Note that the root object is not included in the path so that
+    #
+    #   obj.parent.nil? || obj.root.dot(obj.path) == obj
+    #
+    # is always true
+    #
+    # Note that for this to work, keys may not contain a dots ('.')
+    def path() @path ||= ancestors(true)[1..-1].join(".") end
+
+    # Compare two objects by path
+    def <=>(r) path <=> r.path end
+
+    # A set node is rendered as its key
+    def to_s() key.to_s end
+  end
+
+  class Map < Node
+    def <=>(r) raise NotImplementedError; end
+
+    # A map node is rendered as its object_id
+    def to_s() object_id.to_s end
   end
 end
 
--- spec/hash_tree_spec.rb
+++ spec/hash_tree_spec.rb
@@ -36,7 +36,7 @@ describe HashTree do
       end
     end
     describe "#key" do
-      it "is the unique key identifyin self within a parent" do
+      it "is the unique key identifying self within a parent" do
         expect(root.key).to eq "root"
       end
     end
@@ -59,10 +59,20 @@ describe HashTree do
       end
     end
     describe "#parents" do
-      it "returns a list of parents"
+      it "returns a list of parents" do
+        expect(root.parents).to eq []
+        expect(root.parents(true)).to eq [root]
+        expect(lvl4_3.parents).to eq [lvl3, lvl2, lvl1, root]
+        expect(lvl4_3.parents (true)).to eq [lvl4_3, lvl3, lvl2, lvl1, root]
+      end
     end
     describe "#ancestors" do 
-      it "returns a list of ancestors2"
+      it "returns a list of ancestors" do
+        expect(root.ancestors).to eq []
+        expect(root.ancestors(true)).to eq [root]
+        expect(lvl4_3.ancestors).to eq [root, lvl1, lvl2, lvl3]
+        expect(lvl4_3.ancestors (true)).to eq [root, lvl1, lvl2, lvl3, lvl4_3]
+      end
     end
     describe "#path" do
       it "returns the object's path from the root element" do
@@ -87,10 +97,79 @@ describe HashTree do
         expect(lvl1 <=> lvl2).to eq -1
         expect(lvl3 <=> lvl2).to eq 1
       end
+      it "fails if the nodes are not part of the same tree"
     end
     describe "#to_s" do
       it "renders its #key" do
         expect(lvl1.to_s).to eq lvl1.key.to_s
+      end
+    end
+  end
+
+  describe HashTree::Map do
+    let!(:root) { HashTree::Map.new(nil, "root") }
+    let!(:lvl1) { HashTree::Map.new(root, "LVL1") }
+    let!(:lvl2) { HashTree::Map.new(lvl1, "LVL2") }
+    let!(:lvl3) { HashTree::Map.new(lvl2, "LVL3") }
+    let!(:lvl4_1) { HashTree::Map.new(lvl3, "LVL4_1") }
+    let!(:lvl4_2) { HashTree::Map.new(lvl3, "LVL4_2") }
+    let!(:lvl4_3) { HashTree::Map.new(lvl3, "LVL4_3") }
+
+    describe "#parent" do
+      it "reference the parent" do
+        expect(lvl1.parent).to be root
+      end
+
+      it "is null for the root element" do
+        expect(root.parent).to be nil
+      end
+    end
+    describe "#children" do
+      it "is a hash from key to node" do
+        expect(lvl3.children).to eq({
+          "LVL4_1" => lvl4_1,
+          "LVL4_2" => lvl4_2,
+          "LVL4_3" => lvl4_3
+        })
+      end
+    end
+    describe "#attach" do
+      it "attaches a child node"
+    end
+    describe "#detach" do
+      it "detaches a child node"
+    end
+    describe "#[]" do
+      it "addresses a child node"
+    end
+    describe "#key?" do
+      it "returns true iff key identifies a child"
+    end
+    describe "#root" do
+      it "returns the root element or self if parent is nil" do
+        expect(root.root).to be root
+        expect(lvl4_3.root).to be root
+      end
+    end
+    describe "#parents" do
+      it "returns a list of parents"
+    end
+    describe "#ancestors" do 
+      it "returns a list of ancestors"
+    end
+    describe "#dot" do
+      it "looks up a path and return the matching node" do
+        expect(root.dot("")).to be root
+        expect(root.dot("LVL1")).to be lvl1
+        expect(root.dot("LVL1.LVL2.LVL3.LVL4_3")).to be lvl4_3
+      end
+    end
+    describe "#<=>" do
+      it "compares the nodes recursively"
+    end
+    describe "#to_s" do
+      it "renders its object_id" do
+        expect(lvl1.to_s).to eq lvl1.object_id.to_s
       end
     end
   end
