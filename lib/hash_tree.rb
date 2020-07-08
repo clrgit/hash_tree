28033b298e031df574ae11e014554f5f4263ef4b
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-07-08 22:32:36 +0200

    wip

--- lib/hash_tree.rb
+++ lib/hash_tree.rb
@@ -1,6 +1,100 @@
-require "hash_tree/version"
 
+require "indented_io"
+
+# Simple tree of hashes
+#
+# TODO
+#   o each
+#   o traverse
+#   o #ancestors and #parents as enumerators
+#   o HashTree::Set, HashTree::Map, HashTree::Array
+#
 module HashTree
-  class Error < StandardError; end
-  # Your code goes here...
+  class Set
+    attr_reader :parent
+    attr_reader :children
+    attr_reader :key
+
+    def initialize(parent, key)
+      @parent, @children, @key = parent, {}, key
+      @parent&.send(:attach, self)
+    end
+
+    def attach(child)
+      !@children.key?(child.key) or raise "Duplicate child key: #{child.key.inspect}"
+      child.instance_variable_set(:@parent, self)
+      @children[child.key] = child
+      child.send(:clear_cached_properties)
+    end
+
+    def detach(key)
+      child = children[key]
+      @children.key?(key) or raise "Non-existing child key: #{key.inspect}"
+      child.instance_variable_set(:@parent, nil)
+      @children.delete(key)
+      child.send(:clear_cached_properties)
+    end
+
+    def [](key) @children[key] end
+    def key?(key) @children.key?(key) end
+
+    # The root object or self if parent is nil
+    def root() @root ||= parents.last || self end
+
+    # List of parents up to the root element. If include_self is true, also
+    # include self as the first element
+    def parents(include_self = false)
+      (include_self ? [self] : []) + (@parents ||= (parent&.parents(true) || []))
+    end
+
+    # List of parents from the root element down to parent. If include_self is
+    # true, also include self as the last element
+    def ancestors(include_self = false)
+      (@ancestors ||= parents(false).reverse) + (include_self ? [self] : [])
+    end
+
+    # Unique dot-separated list of keys leading from the root object to 
+    # self. Note that the root object is not included in the path so that
+    #
+    #   obj.parent.nil? || obj.root.dot(obj.path) == obj
+    #
+    # is always true
+    #
+    # Note that for this to work, keys may not contain a dots ('.')
+    def path() @path ||= ancestors(true)[1..-1].join(".") end
+
+    # Recursively lookup object by dot-separated list of names
+    #
+    # Note that for this to work, keys may not contain a dots ('.')
+    def dot(path)
+      path.split(".").inject(self) { |a,e| a[e] or raise "Can't lookup '#{e}' in #{a.path.inspect}" }
+    end
+
+    # Compare two objects by path
+    def <=>(r) path <=> r.path end
+
+    # A node is rendered as its key
+    def to_s() key.to_s end
+
+  private
+    # Recursively clear cached properties like @parents in each node in the
+    # tree. Should be called whenever the node is attached or detached from a
+    # tree
+    #
+    # Note that to speed up the process, it stop recursion when a node has no
+    # cached properties. This is using the fact that the cached properties are
+    # themselves constructed recursively so that if a node has a cached
+    # property, then all its parents will also cache it
+    def clear_cached_properties()
+      if@root || @parents || @ancestors || @path
+        @root = nil
+        @parents = nil
+        @ancestors = nil
+        @path = nil
+        children.values.each { |c| c.clear_cached_properties }
+      end
+    end
+  end
 end
+
+
--- spec/hash_tree_spec.rb
+++ spec/hash_tree_spec.rb
@@ -1,7 +1,99 @@
+
+require 'hash_tree'
+
+include HashTree
+
 describe HashTree do
   it 'has a version number' do
     expect(HashTree::VERSION).not_to be_nil
   end
 
-  it 'does something useful'
+  describe HashTree::Set do
+    let!(:root) { HashTree::Set.new(nil, "root") }
+    let!(:lvl1) { HashTree::Set.new(root, "LVL1") }
+    let!(:lvl2) { HashTree::Set.new(lvl1, "LVL2") }
+    let!(:lvl3) { HashTree::Set.new(lvl2, "LVL3") }
+    let!(:lvl4_1) { HashTree::Set.new(lvl3, "LVL4_1") }
+    let!(:lvl4_2) { HashTree::Set.new(lvl3, "LVL4_2") }
+    let!(:lvl4_3) { HashTree::Set.new(lvl3, "LVL4_3") }
+
+    describe "#parent" do
+      it "reference the parent" do
+        expect(lvl1.parent).to be root
+      end
+
+      it "is null for the root element" do
+        expect(root.parent).to be nil
+      end
+    end
+    describe "#children" do
+      it "is a hash from key to node" do
+        expect(lvl3.children).to eq({
+          "LVL4_1" => lvl4_1,
+          "LVL4_2" => lvl4_2,
+          "LVL4_3" => lvl4_3
+        })
+      end
+    end
+    describe "#key" do
+      it "is the unique key identifyin self within a parent" do
+        expect(root.key).to eq "root"
+      end
+    end
+    describe "#attach" do
+      it "attaches a child node"
+    end
+    describe "#detach" do
+      it "detaches a child node"
+    end
+    describe "#[]" do
+      it "addresses a child node"
+    end
+    describe "#key?" do
+      it "returns true iff key identifies a child"
+    end
+    describe "#root" do
+      it "returns the root element or self if parent is nil" do
+        expect(root.root).to be root
+        expect(lvl4_3.root).to be root
+      end
+    end
+    describe "#parents" do
+      it "returns a list of parents"
+    end
+    describe "#ancestors" do 
+      it "returns a list of ancestors2"
+    end
+    describe "#path" do
+      it "returns the object's path from the root element" do
+        expect(root.path).to eq ""
+        expect(lvl1.path).to eq "LVL1"
+        expect(lvl4_3.path).to eq "LVL1.LVL2.LVL3.LVL4_3"
+      end
+    end
+    describe "#dot" do
+      it "looks up a path and return the matching node" do
+        expect(root.dot("")).to be root
+        expect(root.dot("LVL1")).to be lvl1
+        expect(root.dot("LVL1.LVL2.LVL3.LVL4_3")).to be lvl4_3
+      end
+    end
+    describe "#<=>" do
+      it "compared two objects by path" do
+        alt_root = HashTree::Set.new(nil, "root")
+        alt_lvl1 = HashTree::Set.new(alt_root, "LVL1")
+        alt_lvl2 = HashTree::Set.new(alt_lvl1, "LVL2")
+        expect(alt_lvl2 <=> lvl2).to eq 0
+        expect(lvl1 <=> lvl2).to eq -1
+        expect(lvl3 <=> lvl2).to eq 1
+      end
+    end
+    describe "#to_s" do
+      it "renders its #key" do
+        expect(lvl1.to_s).to eq lvl1.key.to_s
+      end
+    end
+  end
 end
+
+
