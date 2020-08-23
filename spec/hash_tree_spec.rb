aa60fc9de558d707d4f054472f4aac89f41c74a1
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-08-23 22:13:49 +0200

    Changed test hierarchy

--- spec/hash_tree_spec.rb
+++ spec/hash_tree_spec.rb
@@ -4,9 +4,11 @@ require 'hash_tree'
 include HashTree
 
 shared_examples_for "a HashTree::Node" do |klass|
+  # root
   let!(:root) { klass.new(nil, "root") }
-  let!(:lvl1) { klass.new(root, "LVL1") }
-  let!(:lvl2) { klass.new(lvl1, "LVL2") }
+  let!(:lvl1_1) { klass.new(root, "LVL1_1") }
+  let!(:lvl1_2) { klass.new(root, "LVL1_2") }
+  let!(:lvl2) { klass.new(lvl1_1, "LVL2") }
   let!(:lvl3) { klass.new(lvl2, "LVL3") }
   let!(:lvl4_1) { klass.new(lvl3, "LVL4_1") }
   let!(:lvl4_2) { klass.new(lvl3, "LVL4_2") }
@@ -14,7 +16,7 @@ shared_examples_for "a HashTree::Node" do |klass|
 
   describe "#parent" do
     it "reference the parent" do
-      expect(lvl1.parent).to be root
+      expect(lvl1_1.parent).to be root
     end
 
     it "is null for the root element" do
@@ -69,8 +71,8 @@ shared_examples_for "a HashTree::Node" do |klass|
     it "returns a list of parents" do
       expect(root.parents).to eq []
       expect(root.parents(true)).to eq [root]
-      expect(lvl4_3.parents).to eq [lvl3, lvl2, lvl1, root]
-      expect(lvl4_3.parents (true)).to eq [lvl4_3, lvl3, lvl2, lvl1, root]
+      expect(lvl4_3.parents).to eq [lvl3, lvl2, lvl1_1, root]
+      expect(lvl4_3.parents (true)).to eq [lvl4_3, lvl3, lvl2, lvl1_1, root]
     end
   end
 
@@ -78,8 +80,8 @@ shared_examples_for "a HashTree::Node" do |klass|
     it "returns a list of ancestors" do
       expect(root.ancestors).to eq []
       expect(root.ancestors(true)).to eq [root]
-      expect(lvl4_3.ancestors).to eq [root, lvl1, lvl2, lvl3]
-      expect(lvl4_3.ancestors (true)).to eq [root, lvl1, lvl2, lvl3, lvl4_3]
+      expect(lvl4_3.ancestors).to eq [root, lvl1_1, lvl2, lvl3]
+      expect(lvl4_3.ancestors (true)).to eq [root, lvl1_1, lvl2, lvl3, lvl4_3]
     end
   end
 
@@ -107,8 +109,8 @@ shared_examples_for "a HashTree::Node" do |klass|
   describe "#dot" do
     it "looks up a path and return the matching node" do
       expect(root.dot("")).to be root
-      expect(root.dot("LVL1")).to be lvl1
-      expect(root.dot("LVL1.LVL2.LVL3.LVL4_3")).to be lvl4_3
+      expect(root.dot("LVL1_1")).to be lvl1_1
+      expect(root.dot("LVL1_1.LVL2.LVL3.LVL4_3")).to be lvl4_3
     end
   end
 end
@@ -122,8 +124,8 @@ describe HashTree do
     it_should_behave_like "a HashTree::Node", HashTree::Set
 
     let!(:root) { HashTree::Set.new(nil, "root") }
-    let!(:lvl1) { HashTree::Set.new(root, "LVL1") }
-    let!(:lvl2) { HashTree::Set.new(lvl1, "LVL2") }
+    let!(:lvl1_1) { HashTree::Set.new(root, "LVL1_1") }
+    let!(:lvl2) { HashTree::Set.new(lvl1_1, "LVL2") }
     let!(:lvl3) { HashTree::Set.new(lvl2, "LVL3") }
 
     describe "#key" do
@@ -136,22 +138,22 @@ describe HashTree do
       it "moves the node from its current parent to self" do
         root.retach(lvl2)
         expect(lvl2.parent).to be root
-        expect(root.children.values).to eq [lvl1, lvl2]
-        expect(lvl1.children.values).to eq []
+        expect(root.children.values).to eq [lvl1_1, lvl2]
+        expect(lvl1_1.children.values).to eq []
       end
     end
 
     describe "#path" do
       it "returns the object's path from the root element" do
         expect(root.path).to eq ""
-        expect(lvl1.path).to eq "LVL1"
-        expect(lvl3.path).to eq "LVL1.LVL2.LVL3"
+        expect(lvl1_1.path).to eq "LVL1_1"
+        expect(lvl3.path).to eq "LVL1_1.LVL2.LVL3"
       end
     end
 
     describe "#to_s" do
       it "renders its #key" do
-        expect(lvl1.to_s).to eq lvl1.key.to_s
+        expect(lvl1_1.to_s).to eq lvl1_1.key.to_s
       end
     end
   end
@@ -160,11 +162,11 @@ describe HashTree do
     it_should_behave_like "a HashTree::Node", HashTree::Map
 
     let!(:root) { HashTree::Map.new(nil, "root") }
-    let!(:lvl1) { HashTree::Map.new(root, "LVL1") }
+    let!(:lvl1_1) { HashTree::Map.new(root, "LVL1_1") }
 
     describe "#to_s" do
       it "renders its object_id" do
-        expect(lvl1.to_s).to eq lvl1.object_id.to_s
+        expect(lvl1_1.to_s).to eq lvl1_1.object_id.to_s
       end
     end
   end
